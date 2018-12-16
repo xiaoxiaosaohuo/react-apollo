19401c3f1dd1ddd2ce21fdcb930330b871344f12
金 鑫 <jinxin@hualala.com>
2018-12-16 20:10:25 +0800

    学习graphql

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Recipes from "./Recipes";
+import AddRecipe from "./AddRecipe";
+
+const resolvers = {
+  Recipe: {
+    isStarred: parent => {
+      // return false;
+      const starredRecipes =
+        JSON.parse(localStorage.getItem("starredRecipes")) || [];
+      return starredRecipes.includes(parent.id);
+    }
+  },
+  Mutation: {
+    updateRecipeStarred: (_, variables, { getCacheKey, cache }) => {
+      const starredRecipes =
+        JSON.parse(localStorage.getItem("starredRecipes")) || [];
+      if (variables.isStarred) {
+        localStorage.setItem(
+          "starredRecipes",
+          JSON.stringify(starredRecipes.concat([variables.id]))
+        );
+      } else {
+        localStorage.setItem(
+          "starredRecipes",
+          JSON.stringify(
+            starredRecipes.filter(recipeId => recipeId !== variables.id)
+          )
+        );
+      }
+      return {
+        __typename: "Recipe",
+        isStarred: variables.isStarred
+      };
+
+      // const id = getCacheKey({ __typename: "Recipe", id: variables.id });
+      // const fragment = gql`
+      //   fragment isStarredTodo on RecipeItem {
+      //     isStarred
+      //   }
+      // `;
+      // const recipe = cache.readFragment({ fragment, id });
+      // const data = { ...recipe, isStarred: !recipe.isStarred };
+      // cache.writeData({ id, data });
+      // return data;
+    }
+  }
+};
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  clientState: {
+    resolvers
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <AddRecipe />
+        <Recipes />
+      </ApolloProvider>
     );
   }
 }
---
+++ src/Recipes.js
@@ -0,0 +1,88 @@
+import React, { Component, Fragment } from "react";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import recipesQuery from "./recipesQuery";
+
+const updateRecipeStarredMutation = gql`
+  mutation updateRecipeStarred($id: ID!, $isStarred: Boolean!) {
+    updateRecipeStarred(id: $id, isStarred: $isStarred) @client
+  }
+`;
+
+export default class Recipes extends Component {
+    state = {
+        vegetarian: false
+    };
+
+    updateVegetarian = ({ target: { checked } }) => {
+        this.setState({ vegetarian: checked });
+    };
+
+    render() {
+        return <Fragment>
+            <label>
+              <input type="checkbox" checked={this.state.vegetarian} onChange={this.updateVegetarian} />
+              <span>vegetarian</span>
+            </label>
+            <Query query={recipesQuery} variables={{ vegetarian: this.state.vegetarian }} pollInterval={3000}>
+                {({ data, loading, error, refetch }) => {
+                if (loading) return <p>Loading…</p>;
+                if (error) return <p>Something went wrong</p>;
+
+                return(
+                    <Fragment>
+                        <ul>
+                            {data.recipes.map(({ id, title, isStarred }) => (
+                            <li key={id}>
+                                {title}
+                                <Mutation
+                                mutation={updateRecipeStarredMutation}
+                                refetchQueries={[
+                                    {
+                                    query: recipesQuery,
+                                    variables: { vegetarian: false }
+                                    },
+                                    {
+                                    query: recipesQuery,
+                                    variables: { vegetarian: true }
+                                    }
+                                ]}
+                                awaitRefetchQueries={true}
+                                >
+                                {(
+                                    updateRecipeStarred,
+                                    { loading, error }
+                                ) => (
+                                    <button
+                                    className="star-btn"
+                                    style={{
+                                        color: isStarred ? "orange" : "grey",
+                                        animation: loading
+                                        ? "inflate 0.7s ease infinite alternate"
+                                        : "none"
+                                    }}
+                                    onClick={() =>
+                                        updateRecipeStarred({
+                                        variables: {
+                                            id,
+                                            isStarred: !isStarred
+                                        }
+                                        })
+                                    }
+                                    >
+                                    ★ {error && "Failed to update"}
+                                    </button>
+                                )}
+                                </Mutation>
+                            </li>
+                            ))}
+                        </ul>;
+                        <button onClick={() => refetch()}> Refresh Recipes</button>
+                    </Fragment>
+                    ) 
+
+          }}
+            </Query>
+          </Fragment>;
+    }
+}
